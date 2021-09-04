52c3e0b78168b3d00b112cd62f771b20807fd9f7
Ali Zaidi <alihasnain09@hotmail.com>
2021-09-04 15:53:10 -0400

    Add permission handling

--- app/src/main/java/com/zaidiapps/sixcameraradar/MainActivity.java
+++ app/src/main/java/com/zaidiapps/sixcameraradar/MainActivity.java
@@ -1,14 +1,62 @@
 package com.zaidiapps.sixcameraradar;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getName();
+    private final ActivityResultLauncher<String> requestPermission =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    Log.e(TAG, "PERMISSION GRANTED");
+                } else {
+                    Log.e(TAG, "PERMISSION DENIED");
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!checkPermissions()) {
+            requestPermissions();
+        }
+    }
+
+    private boolean checkPermissions() {
+        int permission;
+        if (Build.VERSION.SDK_INT < 29)
+            permission = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        else
+            permission = checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+
+        return permission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_DENIED) {
+            requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        } else if (Build.VERSION.SDK_INT >= 29 &&
+                checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                        == PackageManager.PERMISSION_DENIED) {
+            requestPermission.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+        }
     }
 }
